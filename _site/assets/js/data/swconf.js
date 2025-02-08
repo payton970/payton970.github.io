edc062b8c121d229b170d0001971727d93b218fd
GISEOK RYU <33699776+payton970@users.noreply.github.com>
2025-02-08 00:10:41 -0800

    Modify Ruby version

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1739001741',resources: [
+    cacheName: 'chirpy-1739002029',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
